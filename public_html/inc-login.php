6c4845423b798f996bda47a0228d2d2c7c440186
JavierGonzalez <gonzo@virtualpol.com>
2009-10-07 21:16:57 +0000

    Comentarios añadidos.

--- public_html/inc-login.php
+++ public_html/inc-login.php
@@ -1,35 +1,41 @@
 <?php
-include('config.php');
-include('source/inc-functions.php');
 
-//INIT MICROTIME
+// LOGIN de www.virtualpol.com
+
+include('config.php'); // config raiz
+include('source/inc-functions.php'); // libreria de funciones basicas
+
+// inicia el cronometro para medir el tiempo de carga
 $mtime = explode(' ', microtime()); 
 $tiempoinicial = $mtime[1] + $mtime[0]; 
 
-$date = date('Y-m-d H:i:s');
-$IP = direccion_IP('longip');
-$link = conectar();
+$date = date('Y-m-d H:i:s'); // fija fecha actual $date en formato entendible por MySQL
 
-if (!isset($_SESSION)) { session_start(); }
 
-if (isset($_COOKIE['teorizauser'])) { //NO existe sesion
+$IP = direccion_IP('longip'); // obtiene la IP en formato numérico (longip)
+$link = conectar(); // conecta MySQL
+
+if (!isset($_SESSION)) { session_start(); } // inicia sesion PHP
+
+
+// nucleo del sistema de usuarios, comienza la verificación
+if (isset($_COOKIE['teorizauser'])) {
 	$result = mysql_query("SELECT ID, pass, nick, estado, pols, pais, email, fecha_registro, rechazo_last FROM ".SQL_USERS." WHERE nick = '"  . trim($_COOKIE['teorizauser']) . "' LIMIT 1", $link);
 	while ($row = mysql_fetch_array($result)) { 
-		if (md5(CLAVE.$row['pass']) == $_COOKIE['teorizapass']) { //pass OK
+		if (md5(CLAVE.$row['pass']) == $_COOKIE['teorizapass']) { // cookie pass OK
 			$session_new = true;
 			$pol['nick'] = $row['nick'];
 			$pol['user_ID'] = $row['ID'];
 			$pol['pols'] = $row['pols'];
-
 			$pol['email'] = $row['email'];
-			
-			$_SESSION['pol']['nick'] = $row['nick'];
-			$_SESSION['pol']['user_ID'] = $row['ID'];
-
 			$pol['estado'] = $row['estado'];
 			$pol['pais'] = $row['pais'];
 			$pol['rechazo_last'] = $row['rechazo_last'];
 			$pol['fecha_registro'] = $row['fecha_registro'];
+
+			// variables perdurables en la sesion, solo se guarda el nick e ID de usuario
+			$_SESSION['pol']['nick'] = $row['nick'];
+			$_SESSION['pol']['user_ID'] = $row['ID'];
 		}
 	}  
 }

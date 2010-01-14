af7682ae97a6d5abfc974003fb4118a1ecbba72d
gonzomail <gonzomail@98f5015e-b27f-11de-a73b-b36ed9d36964>
2010-01-14 22:24:57 +0000

    Eliminado el pais VULCAN, D.E.P.

--- public_html/config.php
+++ public_html/config.php
@@ -7,9 +7,9 @@ if ($host[1] != 'virtualpol') { header('HTTP/1.1 301 Moved Permanently'); header
 
 
 // Configuracion Paises y colores
-$vp['paises'] = array('POL', 'Hispania', 'VULCAN');
-$vp['bg'] = array('POL'=>'#E1EDFF', 'VULCAN'=>'#FFD7B3', 'Hispania'=>'#FFFF4F', 'ninguno'=>'#FFFFFF');
-$vp['bg2'] = array('POL'=>'#BFD9FF', 'VULCAN'=>'#FFB3B3', 'Hispania'=>'#D9D900', 'ninguno'=>'#FFFFFF');
+$vp['paises'] = array('POL', 'Hispania');
+$vp['bg'] = array('POL'=>'#E1EDFF', 'Hispania'=>'#FFFF4F', 'ninguno'=>'#FFFFFF');
+$vp['bg2'] = array('POL'=>'#BFD9FF', 'Hispania'=>'#D9D900', 'ninguno'=>'#FFFFFF');
 
 // Configuracion por pais
 switch ($host[0]) {
@@ -17,13 +17,6 @@ switch ($host[0]) {
 case 'pol':
 	define('PAIS', 'POL');
 	define('SQL', 'pol_');
-	define('COLOR_BG', $vp['bg'][PAIS]);
-	define('COLOR_BG2', $vp['bg2'][PAIS]);
-	break;
-
-case 'vulcan':
-	define('PAIS', 'VULCAN');
-	define('SQL', 'vulcan_');
 	define('COLOR_BG', $vp['bg'][PAIS]);
 	define('COLOR_BG2', $vp['bg2'][PAIS]);
 	break;

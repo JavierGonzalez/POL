43d438e405ba3c7b9193b1707691b884dd5225ab
gonzomail <gonzomail@98f5015e-b27f-11de-a73b-b36ed9d36964>
2010-05-08 17:42:16 +0000

    Cambios en el cron para que funcione el nuevo server

--- public_html/source/cron/cron-24h-Hispania.php
+++ public_html/source/cron/cron-24h-Hispania.php
@@ -1,6 +1,9 @@
 <?php 
+if ($_SERVER['REMOTE_ADDR'] != '82.165.128.8') { echo 'Acceso denegado.'; exit; }
+$root_dir = '/var/www/vhosts/virtualpol.com/httpdocs/real/';
+
 
 $_SERVER['HTTP_HOST'] = 'hispania.virtualpol.com';
 
-include('cron-proceso.php');
+include($root_dir.'source/cron/cron-proceso.php');
 ?>
--- public_html/source/cron/cron-proceso-aviso-5m.php
+++ public_html/source/cron/cron-proceso-aviso-5m.php
@@ -1,9 +1,16 @@
 <?php 
+
+
+if ($_SERVER['REMOTE_ADDR'] != '82.165.128.8') { echo 'Acceso denegado.'; exit; }
+$root_dir = '/var/www/vhosts/virtualpol.com/httpdocs/real/';
+
+
 $_SERVER['HTTP_HOST'] = 'pol.virtualpol.com';
 
-include('../../config.php');
-include('../inc-functions.php');
-include('../inc-functions-accion.php');
+
+include($root_dir.'config.php');
+include($root_dir.'source/inc-functions.php');
+include($root_dir.'source/inc-functions-accion.php');
 $link = conectar();
 
 

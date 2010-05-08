3bb88ad384a368827e15c64bc9ad5f0dcb5cf302
gonzomail <gonzomail@98f5015e-b27f-11de-a73b-b36ed9d36964>
2010-05-08 17:49:48 +0000

    Ajuste en cron

--- public_html/source/cron/cron-proceso-aviso-30m.php
+++ public_html/source/cron/cron-proceso-aviso-30m.php
@@ -1,5 +1,5 @@
 <?php 
-if ($_SERVER['REMOTE_ADDR'] != '82.165.128.8') { echo 'Acceso denegado.'; exit; }
+
 $root_dir = '/var/www/vhosts/virtualpol.com/httpdocs/real/';
 
 $_SERVER['HTTP_HOST'] = 'pol.virtualpol.com';

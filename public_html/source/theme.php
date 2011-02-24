9eb3d7b406f344a69afd8f78e596be573a9019c4
gonzomail <gonzomail@98f5015e-b27f-11de-a73b-b36ed9d36964>
2011-02-24 23:59:09 +0000

    Nuevo codigo de Google Analytics para un mejor seguimiento de registros.

--- public_html/source/theme.php
+++ public_html/source/theme.php
@@ -64,6 +64,7 @@ if ($pol['msg'] > 0) { $num_msg = '<b style="font-size:18px;">' . $pol['msg'] . 
 if (($pol['estado'] == 'ciudadano') OR ($pol['estado'] == 'desarrollador')) { // ciudadano
 	$nick_lower = strtolower($pol['nick']);
 	
+	$elecciones = '';
 	if ($pol['config']['elecciones_estado'] == 'normal') {  
 		$elecciones_quedan = duracion(strtotime($pol['config']['elecciones_inicio']) - time());
 		$elecciones = ' <a href="/elecciones/">Elecciones en <b style="font-size:18px;">' . $elecciones_quedan . '</b></a> |'; 
@@ -347,15 +348,16 @@ if ($pol['estado'] == 'desarrollador') {
 
 <script type="text/javascript" src="<?=IMG?>scripts.js"></script>
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-59186-46']);
+_gaq.push(['_setDomainName', '.virtualpol.com']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 </script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-59186-46");
-pageTracker._setDomainName("virtualpol.com");
-pageTracker._trackPageview();
-} catch(err) {}</script>
 
 
 </body>

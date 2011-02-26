07fad8bb1094d6b3a069fbd1990f9c50dd77347f
selvatgi <selvatgi@98f5015e-b27f-11de-a73b-b36ed9d36964>
2011-02-26 11:53:29 +0000

    Adaptado a la estructura actual de la base de datos

--- public_html/source/class-lib.php
+++ public_html/source/class-lib.php
@@ -2,19 +2,18 @@
 class consulta {
 	///////////// PROPIEDADES
 	public $id; //id en la db
-	public $nombre; //titulo de la consulta
+	public $pregunta; //titulo de la consulta
 	public $descripcion; //enunciado de la consulta
 	public $tipo; //sondeo, referendum, parlamento, etc
 	public $respuestas[0]; //array de posibles respuestas
 	private $votos[0]; //array de usuario y opcion votada
 	public $resultados[0]; //array de votos por respuesta
-	public $restriccion_tipo; //nivel, autonomia, partido
-	public $restriccion; //si hay restriccion, cual
 	public $inicio; //hora de inicio de la consulta
 	public $duracion; //tiempo que dura la consulta
 	public $creador; //id del usuario que creo la consulta
-	public $activa; //si la consulta sigue activa o no
-	public $votos_visibles //si se deben conocer los resultados al instante
+	public $estado; //si la consulta sigue activa o no
+	public $num_votos; //numero de gente que ha votado
+	
 	
 	//////////////////// FUNCIONES
 	function __construct($id){
@@ -30,9 +29,10 @@ class consulta {
 		}
 	}
 	function votar($user_id, $resp){ //añade el voto en el array de votos
-		if (($this->ha_votado($user_id == 0) && (in_array($resp, $this->respuestas)) && ($this->activa == 1)){ //si no ha votado, y la respuesta es valida, y la votacion sigue activa
+		if (($this->ha_votado($user_id == 0) && (in_array($resp, $this->respuestas)) && ($this->estado == "ok")){ //si no ha votado, y la respuesta es valida, y la votacion sigue activa
 			$votos[$user_id] = $resp;
 		}
 	}
+}
 	
 		

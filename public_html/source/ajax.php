cdb489202782807784fb1db48fbfd1becdc93f76
JavierGonzalez <gonzo@virtualpol.com>
2011-03-11 17:13:44 +0000

    Nuevo comando /exit para despedirse

--- public_html/source/ajax.php
+++ public_html/source/ajax.php
@@ -171,6 +171,7 @@ LIMIT 1", $link);
 					}
 					break;
 				case 'me': $elmsg = '<b style="margin-left:20px;">' . $_SESSION['pol']['nick'] . '</b> ' . $msg_rest; break;
+				case 'exit': $elmsg = '<b style="margin-left:20px;color:#66004C;">' . $_SESSION['pol']['nick'] . '</b> se marcha, hasta pronto!'; break;
 
 				case 'ayuda':
 				case 'novatos': $elmsg = '<b>[#] ' . $_SESSION['pol']['nick'] . '</b> ofrece ayuda: <a href="http://docs.google.com/present/view?id=ddfcnxdb_15fqwwcpct" target="_blank"><b>Gu&iacute;a Inicial</b></a> - <a href="/doc/empezar-en-'.pais.'/" target="_blank">C&oacute;mo empezar, FAQ</a>.</a>'; break;

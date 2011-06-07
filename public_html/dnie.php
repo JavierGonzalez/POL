a008c6fe5fe83e4a02bbee67e63fd646a47932cf
gonzomail <gonzomail@98f5015e-b27f-11de-a73b-b36ed9d36964>
2011-06-07 13:09:19 +0000

    Cambios menores en integracion de DNIe.

--- public_html/dnie.php
+++ public_html/dnie.php
@@ -60,14 +60,14 @@ Esta linea final no supone ninguna información en claro.
 } elseif (isset($pol['user_ID'])) {
 	$txt .= 'Est&aacute;s autentificado correctamente.';
 } else {
-	$txt .= 'No est&aacute;s registrado.';
+	$txt .= 'No est&aacute;s registrado, debes crear un usuario.';
 }
 
 // Presentacion, diseño y nada más.
 $txt = '
 <style type="text/css">.content { text-align:center; width:500px; margin: 0 auto; padding: 2px 12px 30px 12px; }</style>
 
-<h1>Autentificaci&oacute;n DNIe (FASE ALPHA)</h1>
+<h1>Autentificaci&oacute;n DNIe</h1>
 
 <p>La autentificaci&oacute;n mediante DNIe (y otros certificados) es el futuro, pues permite identificar a una persona fisica con seguridad.</p>
 
@@ -75,12 +75,10 @@ $txt = '
 
 <p>&nbsp;</p>
 
-<p><em>Seguridad:</em> no se almacenar&aacute; ningun dato proporcionado por el DNIe u otro certificado en ninguna parte del sistema. Tan solo se almacena una miniaturizaci&oacute;n irreversible de esta informaci&oacute;n. De esta forma incluso ante el peor ataque posible (acceso a contrase&ntilde;as, claves y base de datos) no se podr&iacute;a obtener informaci&oacute;n alguna. La pasarela de autentificaci&oacute;n se conf&iacute;a totalmente a una empresa importante del sector llamada Tractis.</p>';
+<p><em>Seguridad:</em> no se almacenar&aacute; ningun dato proporcionado por el DNIe u otro certificado en ninguna parte del sistema. Tan solo se almacena una miniaturizaci&oacute;n irreversible de esta informaci&oacute;n. De esta forma incluso ante el peor ataque posible (acceso a contrase&ntilde;as, claves y base de datos) no se podr&iacute;a obtener informaci&oacute;n alguna. La pasarela de autentificaci&oacute;n se conf&iacute;a totalmente a una empresa importante del sector llamada <a href="http://www.tractis.com/">Tractis</a>.</p>
 
-// Carga el diseño completo de VirtualPol. Mucho HTML, CSS y poco más.
+<p>Puedes ver el codigo fuente de <a href="http://code.google.com/p/virtualpol/source/browse/trunk/public_html/dnie.php?spec=svn572&r=572">esta aplicaci&oacute;n</a>.</p>';
+
+// Carga el dise&ntilde;o completo de VirtualPol. Mucho HTML, CSS y poco m&aacute;s.
 include('theme.php');
 ?>
-
-
-
-

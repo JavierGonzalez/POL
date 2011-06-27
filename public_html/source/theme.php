f7156937b4a24551fb0abc7a6f134fa0d42f297e
selvatgi <selvatgi@gmail.com>
2011-06-27 13:44:47 +0000

    mejora de lo anterior

--- public_html/source/theme.php
+++ public_html/source/theme.php
@@ -162,7 +162,7 @@ function vlgn (objeto) { if ((objeto.value == "Usuario") || (objeto.value == "12
 						<li><a href="/log-eventos/">Log de eventos</a></li>
 					</ul>
 			</li>
-			if ($pol['pais'] == "VP"){ ?>
+		<?php	if ($pol['pais'] == "VP"){ ?>
 			<li>
 				<a href="#">Pol&iacute;tica</a>
 				<ul>

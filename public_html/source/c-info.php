cf51c41de03af5ca201847757a9af2d129fe4b48
JavierGonzalez <gonzo@virtualpol.com>
2012-01-19 01:05:22 +0000

    Arreglos varios.

--- public_html/source/c-info.php
+++ public_html/source/c-info.php
@@ -4,6 +4,87 @@ include('inc-login.php');
 switch ($_GET['a']) {
 
 
+
+case 'seguir':
+
+
+function red_social($red, $ID) {
+
+	if ($red == 'twitter') {
+		return '<a href="https://twitter.com/'.$ID.'" class="twitter-follow-button" data-show-count="false" data-lang="es" data-size="large">Segui @'.$ID.'</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+	}
+
+}
+
+$txt_title = 'Sala de seguir';
+$txt .= '<h1>Sala de Seguir</h1>
+<br />
+<table border="0">';
+
+
+$txt .= '<tr>
+<td align="right" colspan="2"><em>VirtualPol</em></td>
+<td>'.red_social('twitter', 'VirtualPol').'</td>
+<td></td>
+</tr>';
+
+
+if (ASAMBLEA) {
+	$txt .= '<tr>
+<td align="right" colspan="2"><em>Asamblea Virtual</em></td>
+<td>'.red_social('twitter', 'AsambleaVirtuaI').'</td>
+<td></td>
+</tr>';
+}
+
+
+$txt .= '<tr>
+<th>Ciudadano</th>
+<th>Confianza</th>
+<th>Twitter</th>
+</tr>';
+
+	$dias = 1;
+	$result = mysql_query("SELECT ID, nick, datos, voto_confianza
+FROM users
+WHERE estado = 'ciudadano' AND pais = '".PAIS."' AND datos != ''
+ORDER BY voto_confianza DESC
+LIMIT 10000", $link);
+	while($r = mysql_fetch_array($result)) { 
+
+
+		$datos_array = explode('][', $r['datos']);
+
+
+		// TWITTER
+		$twitter_ID = false;
+		$twitter = false;
+		if ($datos_array[1] != '') {
+			foreach (explode('/', '/'.$datos_array[1]) AS $elemento) { $twitter_ID = $elemento; }
+			$twitter_ID = str_replace('#', '', str_replace('@', '', $twitter_ID));
+			if (strlen($twitter_ID) >= 3) { 
+				$twitter = red_social('twitter', $twitter_ID); 
+			}
+		}
+
+
+
+		
+		if ($twitter) {
+			$txt .= '<tr>
+<td align="right">'.crear_link($r['nick']).'</td>
+<td align="right">'.confianza($r['voto_confianza']).'</td>
+<td>'.($r['ID']==$pol['user_ID']?'':$twitter).'</td>
+<td></td>
+</tr>';
+		}
+
+	}
+	$txt .= '</table>';
+
+	break;
+
 case 'expiracion':
 
 

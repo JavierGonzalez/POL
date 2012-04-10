96c1700baae7b6769ee2cebc8d692f2d231b9833
J.A. Nache <nache.nache@gmail.com>
2012-04-11 01:18:16 +0200

    Adecentando el html ya hecho y algunas mejoras en el manejador de themes

--- public_html/install/class/class-theme.php
+++ public_html/install/class/class-theme.php
@@ -20,6 +20,11 @@ class theme{
 	}
 
 	private function process_vars(){
+		// esta funciones es mejorable,
+		// quizas se pueda hacer todo de una pasada evitando
+		// ciclos y consumo de ram 
+
+		//parseamos los includes
 		$found=array();
 		while(ereg('\{INCLUDE\:([^}]+'.THEME_MIME.')\}',$this->html,$found)){
 			if(file_exists(THEME."/".$found[1])){
@@ -32,8 +37,14 @@ class theme{
 				$this->html=str_replace($found[0], "", $this->html);
 			}
 		}
-		unset($found);
-		$this->html=str_replace($this->search,$this->replace,$this->html);
+		unset($found); //ayudamos al recolector de basura
+		$this->html=str_replace($this->search,$this->replace,$this->html); //Las variables asignadas desde la app
+
+		//ahora las variables del sistema
+		$this->html=str_replace("{THEMEPATH}",THEME,$this->html);
+
+		//por ultimo, variables inexistentes, fuera
+		$this->html=preg_replace("/\{[^\}]+\}/","",$this->html);
 	}
 
 }//end class

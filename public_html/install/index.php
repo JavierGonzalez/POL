f87684da769bdd3f831fa8bccdcc40758f7d282f
J.A. Nache <nache.nache@gmail.com>
2012-04-12 00:44:36 +0200

    Avances en el instalador

--- public_html/install/index.php
+++ public_html/install/index.php
@@ -9,6 +9,7 @@ switch($_GET['step']){
 		if(isset($_POST['send']))
 		{
 			$_SESSION['i_domain']=$_POST['domain'];
+			$_SESSION['i_ctmail']=$_POST['ctmail'];
 			$_SESSION['i_dbname']=$_POST['dbname'];
 			$_SESSION['i_dbuser']=$_POST['dbuser'];
 			$_SESSION['i_dbhost']=$_POST['dbhost'];
@@ -21,6 +22,7 @@ switch($_GET['step']){
 			}
 				
 			if( strlen($_SESSION['i_domain']) > 0 &&
+			strlen($_SESSION['i_ctmail']) > 0 &&
 			strlen($_SESSION['i_dbname']) > 0 &&
 			strlen($_SESSION['i_dbuser']) > 0 &&
 			strlen($_SESSION['i_dbpass']) > 0 &&
@@ -58,10 +60,45 @@ switch($_GET['step']){
 								'$mysql_pass = \''.$_SESSION["i_dbpass"].'\';'
 							), 
 							$conf_pwd);
-					if(file_put_contents("../config-pwd.php",$conf_pwd) === FALSE){
+					if(file_put_contents("../config-pwd.php",$conf_pwd) === FALSE)
+					{
 						$theme->addvar("{ERROR}", "No se pudo escribir en config-pwd.php, compruebe los permisos.");
-					}else{
-						header("Location: ?step=1");
+					}
+					else
+					{
+
+						//aqui escribimos config.php
+						$conf = file_get_contents("../config-sample.php");
+						$conf = str_replace(
+								array(
+									'define(\'DOMAIN\', \'virtualpol.com\');',
+									'define(\'CONTACTO_EMAIL\', \'desarrollo@virtualpol.com\');'
+								),
+								array(
+									'define(\'DOMAIN\', \''.$_SESSION["domain"].'\');',
+									'define(\'CONTACTO_EMAIL\', \''.$_SESSION["ctmail"].'\');'
+								),
+								$conf);
+						if(file_put_contents("../config.php",$conf) === FALSE)
+						{
+							$theme->addvar("{ERROR}", "No se pudo escribir en config.php, compruebe los permisos.");
+						}
+						else
+						{
+
+							//comprobamos que todo se ha configurado correctamente
+							include("../config-pwd.php");
+							$link = conectar();
+							if( !$link )
+							{
+								$theme->addvar("{ERROR}", "Error: Parece que los valores de conexi&oacute;n no se han escrito correctamente ".mysql_error());
+							}
+							else
+							{
+								mysql_close($link);
+								header("Location: ?step=1");
+							}
+						}
 					}
 				}
 				
@@ -72,6 +109,11 @@ switch($_GET['step']){
 			(isset($_SESSION['i_domain'])) ? 
 			$_SESSION['i_domain'] : 
 			$_SERVER['SERVER_NAME']);
+
+		$theme->addvar("{CTMAIL}",
+			(isset($_SESSION['i_ctmail'])) ?
+			$_SESSION['i_ctmail'] :
+			"desarrollo@virtualpol.com");
 
 		$theme->addvar("{DBNAME}",
 			(isset($_SESSION['i_dbname'])) ? 
@@ -85,6 +127,7 @@ switch($_GET['step']){
 			(isset($_SESSION['i_dbhost'])) ? 
 			$_SESSION['i_dbhost'] : "localhost");
 
+
 		$theme->addvar("{DBPASS}","");
 
 		$theme->addvar("{TITLE}","step0");
@@ -93,6 +136,92 @@ switch($_GET['step']){
 		
 
 	case 1:
+
+		include("../config-pwd.php");
+		$link = conectar();
+		if( !$link ){
+			$theme->addvar("{ERROR}", "Error: Parece que los valores de conexi&oacute;n no se han escrito correctamente ".mysql_error());
+		}else{
+
+			$result = mysql_query("SELECT count(table_name) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '".$_SESSION["i_dbname"]."'", $link);
+			if( mysql_num_rows ($result) > 0 ){
+				$theme->addvar("{ERROR}", "Parece que su base de datos contiene algunas tablas. Si continua, las tablas de VirtualPol ser&aacute;n reseteadas. Perder&aacute; todos los datos almacenados. Haga Backup!");
+			}
+
+			if(isset($_POST['send'])){
+				
+				$vp_tables = array
+						(	
+							"15m_foros",
+							"15m_foros_hilos",
+							"15m_foros_msg",
+							"atlantis_foros",
+							"atlantis_foros_hilos",
+							"atlantis_foros_msg",
+							"cargos",
+							"cargos_users",
+							"cat",
+							"chats",
+							"chats_msg",
+							"config",
+							"cuentas",
+							"docs",
+							"empresas",
+							"empresas_acciones",
+							"examenes",
+							"examenes_preg",
+							"expulsiones",
+							"grupos",
+							"hechos",
+							"hispania_foros",
+							"hispania_foros_hilos",
+							"hispania_foros_msg",
+							"kicks",
+							"log",
+							"mapa",
+							"mensajes",
+							"notificaciones",
+							"partidos",
+							"partidos_listas",
+							"pol_foros",
+							"pol_foros_hilos",
+							"pol_foros_msg",
+							"pujas",
+							"referencias",
+							"rssv_foros",
+							"rssv_foros_hilos",
+							"rssv_foros_msg",
+							"stats",
+							"transacciones",
+							"users",
+							"votacion",
+							"votacion_votos",
+							"votos",
+							"vp_foros",
+							"vp_foros_hilos",
+							"vp_foros_msg",
+							"vulcan_foros",
+							"vulcan_foros_hilos",
+							"vulcan_foros_msg"
+						);
+
+				foreach( $vp_tables as $vp_table ){
+					$runinfo.="- Eliminando $vp_table <br />";
+					if( ! mysql_query("drop table if exists $vp_table", $link)){ $runinfo.="********".mysql_error()."********<br />"; }
+
+				}
+
+				//aqui instalamos las tablas
+				$vp_tables=""; //liberando ram
+
+
+				$theme->addvar("{RUNINFO}",$runinfo);
+
+			}
+		}
+
+
+		$theme->addvar("{TITLE}","step1");
 		$theme->putfile("step1");
 		break;
 	default:

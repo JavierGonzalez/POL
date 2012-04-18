ac157327589e4f52b05e283ec179f1eb37fa07c7
JavierGonzalez <gonzo@virtualpol.com>
2012-04-18 22:06:14 +0200

    Cabeceras de licencia y derechos de autor

--- public_html/dnie.php
+++ public_html/dnie.php
@@ -1,4 +1,12 @@
 <?php
+/* The source code packaged with this file is Free Software, Copyright (C) 2008 by
+** Javier González González <desarrollo AT virtualpol.com> <gonzomail AT gmail.com>
+** It's licensed under the GNU GENERAL PUBLIC LICENSE v3 unless stated otherwise.
+** You can get copies of the licenses here: http://www.gnu.org/licenses/gpl.html
+** The source: http://www.virtualpol.com/codigo - TOS: http://www.virtualpol.com/TOS
+** VirtualPol, The first Democratic Social Network - http://www.virtualpol.com
+*/
+
 // Conecta con la base de datos, define constantes, carga el sistema de usuarios de VP, hace un par de gestiones rutinarias.
 include('inc-login.php');
 

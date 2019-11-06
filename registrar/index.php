a4a833330bd492d4b4931ded7663781852fff630
JavierGonzalez <gonzo@virtualpol.com>
2019-11-06 11:45:04 +0100

    Registro fix

--- registrar/index.php
+++ registrar/index.php
@@ -80,7 +80,8 @@ case 'registrar': //CHECK
 		$bloquear_registro = false;
 
 		// Bloquea registro si la IP coincide con otro expulsado
-		$margen = date('Y-m-d H:i:s', time() - (60*60)); // 1 h
+		/*
+        $margen = date('Y-m-d H:i:s', time() - (60*60)); // 1 h
 		$result = sql("SELECT ID FROM users WHERE (estado = 'expulsadoNO' OR fecha_registro > '".$margen."') AND (IP = '".direccion_IP('longip')."' OR hosts LIKE '%".direccion_IP()."%') LIMIT 1");
 		while ($r = r($result)) { $bloquear_registro = true; }
 
@@ -88,72 +89,73 @@ case 'registrar': //CHECK
 			$la_IP = trim($la_IP);
 			if (stristr(' '.direccion_IP(), ' '.explodear(' ', $la_IP, 0))) { $bloquear_registro = true; }
 		}
+        */
 
 		
 		if ($bloquear_registro === false) {
 
 
-		if ((CAPTCHA_REGISTRO == false) OR (animal_captcha_check($_POST['animal']) == true)) {
+            if ((CAPTCHA_REGISTRO == false) OR (animal_captcha_check($_POST['animal']) == true)) {
 
-			//CONTROL: solo letras y numeros en nick
-			if ((onlynumbers($nick) == true) AND (!in_array($nick, $nicks_prohibidos))) { 
+                //CONTROL: solo letras y numeros en nick
+                if ((onlynumbers($nick) == true) AND (!in_array($nick, $nicks_prohibidos))) { 
 
-				//CONTROL: contraseñas
-				$rn = $_POST['repid'];
-				if (($pass1) && ($pass1 === $pass2)) {
-					if (comprobar_email($email) == true) {
+                    //CONTROL: contraseñas
+                    $rn = $_POST['repid'];
+                    if (($pass1) && ($pass1 === $pass2)) {
+                        if (comprobar_email($email) == true) {
 
-						$result = sql("SELECT ID FROM users WHERE email = '$email' LIMIT 1");
-						while ($r = r($result)) { $email_existe = $r['ID'];}
+                            $result = sql("SELECT ID FROM users WHERE email = '$email' LIMIT 1");
+                            while ($r = r($result)) { $email_existe = $r['ID'];}
 
-						if (!$email_existe) { //el email esta libre
-							if ((strlen($nick) >= 3) AND (strlen($nick) <= 14)) {
+                            if (!$email_existe) { //el email esta libre
+                                if ((strlen($nick) >= 3) AND (strlen($nick) <= 14)) {
 
-								$result = sql("SELECT ID FROM users WHERE nick = '".$nick."' LIMIT 1");
-								while ($r = r($result)) { $nick_existe = $r['ID'];}
+                                    $result = sql("SELECT ID FROM users WHERE nick = '".$nick."' LIMIT 1");
+                                    while ($r = r($result)) { $nick_existe = $r['ID'];}
 
-								$result = sql("SELECT tiempo FROM expulsiones WHERE tiempo = '".$nick."' AND estado = 'expulsado' LIMIT 1");
-								while ($r = r($result)) { $nick_expulsado_existe = $r['tiempo']; }
+                                    $result = sql("SELECT tiempo FROM expulsiones WHERE tiempo = '".$nick."' AND estado = 'expulsado' LIMIT 1");
+                                    while ($r = r($result)) { $nick_expulsado_existe = $r['tiempo']; }
 
-								if ((!$nick_existe) AND (!$nick_expulsado_existe)) { //si el nick esta libre
-									$longip = ip2long($_SERVER['REMOTE_ADDR']);
+                                    if ((!$nick_existe) AND (!$nick_expulsado_existe)) { //si el nick esta libre
+                                        $longip = ip2long($_SERVER['REMOTE_ADDR']);
 
 
-									//Si existe referencia IP
-									$afiliacion = 0;
-									$result = sql("SELECT ID, user_ID, (SELECT nick FROM users WHERE ID = referencias.user_ID LIMIT 1) AS nick FROM referencias WHERE IP = '".$longip."' LIMIT 1");
-									while($r = r($result)){ 
-										$afiliacion = $r['user_ID'];
-										$ref = ' (ref: ' . crear_link($r['nick']) . ')';
-									}
-									
-									// gen API pass
-									$api_pass = substr(md5(mt_rand(1000000000,9999999999)), 0, 12);
+                                        //Si existe referencia IP
+                                        $afiliacion = 0;
+                                        $result = sql("SELECT ID, user_ID, (SELECT nick FROM users WHERE ID = referencias.user_ID LIMIT 1) AS nick FROM referencias WHERE IP = '".$longip."' LIMIT 1");
+                                        while($r = r($result)){ 
+                                            $afiliacion = $r['user_ID'];
+                                            $ref = ' (ref: ' . crear_link($r['nick']) . ')';
+                                        }
+                                        
+                                        // gen API pass
+                                        $api_pass = substr(md5(mt_rand(1000000000,9999999999)), 0, 12);
 
-									//crea el ciudadano
-									if (strlen($pass1) != 32) { 
-										$pass_md5 = pass_key($pass1, 'md5');
-										$pass_sha = pass_key($pass1);
-									}
-									
-									sql("INSERT INTO users 
+                                        //crea el ciudadano
+                                        if (strlen($pass1) != 32) { 
+                                            $pass_md5 = pass_key($pass1, 'md5');
+                                            $pass_sha = pass_key($pass1);
+                                        }
+                                        
+                                        sql("INSERT INTO users 
 (nick, pols, fecha_registro, fecha_last, partido_afiliado, estado, nivel, email, num_elec, online, fecha_init, ref, ref_num, api_pass, api_num, IP, nota, avatar, text, cargo, visitas, paginas, nav, voto_confianza, confianza_historico, pais, pass, pass2, host, IP_proxy, dnie_check, bando, nota_SC, fecha_legal) 
 VALUES ('".$nick."', '0', '".$date."', '".$date."', '', 'turista', '1', '" . strtolower($email) . "', '0', '0', '" . $date . "', '".$afiliacion."', '0', '".$api_pass."', '0', '" . $IP . "', '0.0', 'false', '', '', '0', '0', '" . $_SERVER['HTTP_USER_AGENT'] . "', '0', '0', 'ninguno', '".$pass_md5."', '".$pass_sha."', '".@gethostbyaddr($_SERVER['REMOTE_ADDR'])."', '".ip2long($_SERVER['HTTP_X_FORWARDED_FOR'])."', null, null, '".((($_POST['nick_clon']=='')||(strtolower($_POST['nick_clon'])=='no'))?'':'Comparte con: '.$_POST['nick_clon'])."', '".$date."')");
-									$result = sql("SELECT ID FROM users WHERE nick = '".$nick."' LIMIT 1");
-									while($r = r($result)){ $new_ID = $r['ID']; }
-									
-									if (!$_COOKIE['trz']) {
-										$_COOKIE['trz'] = round(microtime(true)*10000);
-										setcookie('trz', $_COOKIE['trz'], (time()+(86400*365)), '/', USERCOOKIE);
-									}
+                                        $result = sql("SELECT ID FROM users WHERE nick = '".$nick."' LIMIT 1");
+                                        while($r = r($result)){ $new_ID = $r['ID']; }
+                                        
+                                        if (!$_COOKIE['trz']) {
+                                            $_COOKIE['trz'] = round(microtime(true)*10000);
+                                            setcookie('trz', $_COOKIE['trz'], (time()+(86400*365)), '/', USERCOOKIE);
+                                        }
 
-									users_con($new_ID, $_REQUEST['extra'], 'login');
+                                        users_con($new_ID, $_REQUEST['extra'], 'login');
 
-									if ($ref) {
-										sql("UPDATE referencias SET new_user_ID = '" . $new_ID . "' WHERE IP = '" . $longip . "' LIMIT 1");
-									}
+                                        if ($ref) {
+                                            sql("UPDATE referencias SET new_user_ID = '" . $new_ID . "' WHERE IP = '" . $longip . "' LIMIT 1");
+                                        }
 
-									$mensaje = '<p>'._('Hola').' '.$nick.':</p>
+                                        $mensaje = '<p>'._('Hola').' '.$nick.':</p>
 
 <p>'._('Para terminar el registro debes validar tu usuario. Simplemente tienes que entrar en la siguiente dirección web').':</p>
 
@@ -161,17 +163,17 @@ VALUES ('".$nick."', '0', '".$date."', '".$date."', '', 'turista', '1', '" . str
 
 <p>'._('¡Esto es todo!').'</p>';
 
-									enviar_email(null, _('Verificar nuevo usuario').': '.$nick, $mensaje, $email);
+                                        enviar_email(null, _('Verificar nuevo usuario').': '.$nick, $mensaje, $email);
 
-									$registro_txt .= '<p><span style="color:green;"><b>'._('¡Correcto!').'</b></span>. '._('Tu usuario ha sido creado exitosamente').'.</p><p>'._('Último paso. Ahora <b>debes revisar tu email</b>, te hemos enviado un email para validar tu usuario').'.</p><p><em>* '._('Por favor, rescata el email si lo encuentras en la carpeta de spam. Esta medida es por seguridad').'</em>.</p>';
+                                        $registro_txt .= '<p><span style="color:green;"><b>'._('¡Correcto!').'</b></span>. '._('Tu usuario ha sido creado exitosamente').'.</p><p>'._('Último paso. Ahora <b>debes revisar tu email</b>, te hemos enviado un email para validar tu usuario').'.</p><p><em>* '._('Por favor, rescata el email si lo encuentras en la carpeta de spam. Esta medida es por seguridad').'</em>.</p>';
 
-								} else {$nick = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El nick está ocupado, por favor elige otro').'</p>';}
-							} else {$nick = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El nick debe tener entre 3 y 14 caracteres').'</p>';}
-						} else {$email = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('Dirección de email en uso').', <a href="'.REGISTRAR.'login.php?a=recuperar-pass"><b>'._('recupera tu usuario').'</b></a></p>';}
-					} else {$email = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El email no es valido').'</p>';}
-				} else { $pass1 = ''; $pass2 = '';  $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('Debes escribir la misma contraseña dos veces').'</p>';}
-			} else { $pass1 = ''; $pass2 = '';  $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El nick solo puede tener letras, numeros y el caracter: "_". La inicial nunca debe ser un numero').'.</p>';}
-		} else { $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('No has acertado captcha').'</p>'; }
+                                    } else {$nick = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El nick está ocupado, por favor elige otro').'</p>';}
+                                } else {$nick = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El nick debe tener entre 3 y 14 caracteres').'</p>';}
+                            } else {$email = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('Dirección de email en uso').', <a href="'.REGISTRAR.'login.php?a=recuperar-pass"><b>'._('recupera tu usuario').'</b></a></p>';}
+                        } else {$email = ''; $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El email no es valido').'</p>';}
+                    } else { $pass1 = ''; $pass2 = '';  $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('Debes escribir la misma contraseña dos veces').'</p>';}
+                } else { $pass1 = ''; $pass2 = '';  $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('El nick solo puede tener letras, numeros y el caracter: "_". La inicial nunca debe ser un numero').'.</p>';}
+            } else { $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('No has acertado captcha').'</p>'; }
 
 		} else { $verror .= '<p class="vmal"><b>'._('Error').'</b>: '._('Se ha activado un bloqueo a tu conexión. Si crees que puede ser un error contacta').': '.CONTACTO_EMAIL.'</p>'; }
 

d1a91d89b6855285e7fec61a0b72feb290e8ecc3
Bradduk <david@DAVID-PORTATIL.localdomain>
2020-09-19 23:30:27 +0200

    Gestion correcta de usuarios expulsados

--- cron/index.php
+++ cron/index.php
@@ -331,7 +331,40 @@ sql_old("DELETE FROM notificaciones WHERE time < '".tiempo(10)."'");
 // ELIMINAR users_con
 sql_old("DELETE FROM users_con WHERE time < '".tiempo(30)."'");
 
+//Issue 140 - Eliminar posesiones de usuarios expulsados
 
+
+
+$result3 = sql_old("SELECT IP, pols, nick, ID, ref, estado,
+".(ECONOMIA?"(SELECT SUM(pols) FROM cuentas WHERE pais = '".PAIS."' AND user_ID = '".$pol['user_ID']."')":"estado")." AS pols_cuentas 
+FROM users 
+WHERE estado = 'expulsado' AND pais <> 'ninguno'
+LIMIT 1");
+while($r3 = r($result3)) {
+	$user_ID = $r3['ID']; 
+	$estado = $r3['estado']; 
+	$pols = ($r3['pols'] + $r3['pols_cuentas']); 
+	$nick = $r3['nick']; 
+	$ref = $r3['ref']; 
+	$IP = $r3['IP'];
+
+	evento_log('Eliminando propiedades de usuario ('.$nick.') previamente expulsado.');
+	pols_transferir($pols, $user_ID, '-1', 'Eliminación de usuario ');
+
+	sql_old("DELETE FROM empresas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+	sql_old("DELETE FROM cuentas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+	sql_old("DELETE FROM mapa WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+	sql_old("DELETE FROM pujas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+
+	sql_old("DELETE FROM cargos_users WHERE user_ID = '".$user_ID."'");
+	sql_old("DELETE FROM partidos_listas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+	sql_old("DELETE FROM partidos WHERE pais = '".PAIS."' AND ID_presidente = '".$user_ID."'");
+
+
+	sql_old("UPDATE users SET estado = 'expulsado', pais = 'ninguno', nivel = '1', cargo = '0', cargos = '', examenes = '', nota = '0.0', pols = '0.0' WHERE ID = '".$user_ID."' LIMIT 1");
+}
+
+//Fin eliminación posesiones.
 
 /* Expiraciones:
 NUNCA
--- registrar/login/borrar-usuario.php
+++ registrar/login/borrar-usuario.php
@@ -2,7 +2,32 @@
 
 
 if ($_POST['nick'] == $pol['nick']) { 
-    evento_log('Eliminación de usuario permanente y voluntaria.');
-    sql_old("UPDATE users SET estado = 'expulsado' WHERE ID = '".$pol['user_ID']."' LIMIT 1"); 
+	$result3 = sql_old("SELECT IP, pols, nick, ID, ref, estado,
+    ".(ECONOMIA?"(SELECT SUM(pols) FROM cuentas WHERE pais = '".PAIS."' AND user_ID = '".$pol['user_ID']."')":"estado")." AS pols_cuentas 
+    FROM users 
+    WHERE ID = '".$pol['user_ID']."' AND estado = 'ciudadano' AND pais = '".PAIS."'
+    LIMIT 1");
+	while($r3 = r($result3)) {
+		$user_ID = $r3['ID']; 
+		$estado = $r3['estado']; 
+		$pols = ($r3['pols'] + $r3['pols_cuentas']); 
+		$nick = $r3['nick']; 
+		$ref = $r3['ref']; 
+		$IP = $r3['IP'];
+	}
+    evento_log('Eliminación de usuario ('.$nick.') permanente y voluntaria.');
+    pols_transferir($pols, $user_ID, '-1', 'Eliminación de usuario ');
+    
+    sql_old("DELETE FROM empresas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+    sql_old("DELETE FROM cuentas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+    sql_old("DELETE FROM mapa WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+    sql_old("DELETE FROM pujas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+
+    sql_old("DELETE FROM cargos_users WHERE user_ID = '".$user_ID."'");
+    sql_old("DELETE FROM partidos_listas WHERE pais = '".PAIS."' AND user_ID = '".$user_ID."'");
+    sql_old("DELETE FROM partidos WHERE pais = '".PAIS."' AND ID_presidente = '".$user_ID."'");
+
+
+    sql_old("UPDATE users SET estado = 'expulsado', pais = 'ninguno', nivel = '1', cargo = '0', cargos = '', examenes = '', nota = '0.0', pols = '0.0', rechazo_last = '".$date."' WHERE ID = '".$pol['user_ID']."' LIMIT 1");
 }
 redirect('/');
